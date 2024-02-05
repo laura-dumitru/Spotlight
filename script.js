1edd32c9ddde5434bdeb6d6776f35d85530e3060
Laura Dumitru <lauradumitru88@icloud.com>
2024-02-05 19:25:54 +0000

    Fixed issue with icon not changing

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const overlay = document.querySelector(".overlay");
 const on = document.querySelector(".on");
 const off = document.querySelector(".off");
+const icon = document.querySelector(".icon");
+
+lucide.createIcons();
 
 overlay.addEventListener("mousemove", touchStart);
 
@@ -12,21 +15,21 @@ function touchStart(e) {
   e.preventDefault();
   const x = e.offsetX;
   const y = e.offsetY;
-  //console.log(x);
-  //console.log(y);
-  //console.log(e);
   overlay.style.setProperty("--mouse-x", `${x}px`); //setting the x property to the mouse position
   overlay.style.setProperty("--mouse-y", `${y - 100}px`);
 }
-let isMoved = false;
 
+let isMoved = false;
 on.addEventListener("click", () => {
   if (isMoved === true) {
-    overlay.style.display = "none";
-    on.style.marginLeft = "calc(100% - 34px)";
-  } else {
     overlay.style.display = "inherit"; // Show overlay
-    on.style.marginLeft = "2px"; //
+    on.style.marginLeft = 0; // Move left
+    on.innerHTML = `<i class="icon" data-lucide="flashlight"></i>`;
+  } else {
+    overlay.style.display = "none"; // Hide overlay
+    on.style.marginLeft = "calc(100% - 2.8em)"; // Move right
+    on.innerHTML = `<i class="icon" data-lucide="flashlight-off"></i>`;
   }
   isMoved = !isMoved;
+  lucide.createIcons();
 });
