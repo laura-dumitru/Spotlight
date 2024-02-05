5949149a9d1c8ceda550b7125ae1cc75346317ca
Laura Dumitru <lauradumitru88@icloud.com>
2024-02-05 16:19:45 +0000

    Added event listener on buttons

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const overlay = document.querySelector(".overlay");
+const on = document.querySelector(".on");
+const off = document.querySelector(".off");
 
 overlay.addEventListener("mousemove", touchStart);
 
@@ -13,6 +15,15 @@ function touchStart(e) {
   //console.log(x);
   //console.log(y);
   //console.log(e);
-  overlay.style.setProperty("--mouse-x", `${x}px`);
+  overlay.style.setProperty("--mouse-x", `${x}px`); //setting the x property to the mouse position
   overlay.style.setProperty("--mouse-y", `${y - 64}px`);
 }
+
+/*off.addEventListener("click", () => {
+  overlay.style.display = "none";
+});*/
+
+on.addEventListener("click", () => {
+  //overlay.style.display = "inherit";
+  overlay.style.display = "none";
+});
